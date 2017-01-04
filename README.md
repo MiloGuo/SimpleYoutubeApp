b35efb7839d36c5193bbe414b040d6712b845289
Milo Guo <miloguoqi@gmail.com>
2017-01-04 11:15:41 -0800

    Added js files
    Added js files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple Youtube App (pure react app)
+
+
+Git clone or extract the contents of the zip file, then open your terminal, change to the project directory, and:
+
+```
+	> npm install
+	> npm start
+```
---
+++ src/index.js
@@ -0,0 +1,50 @@
+import _ from 'lodash';
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyCA6Rl85OXjbbh6jObJ2sfariTeqQaf84s';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+	console.log(data);
+})
+
+
+class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+		this.videoSearch('korean');
+
+	}
+
+	videoSearch(term){
+		YTSearch({key: API_KEY, term: term}, (videos)=>{
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+
+	}
+	render() {
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)},300);
+		return (
+			<div>
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
+			</div>
+		);
+	}
+
+}
+
+ReactDom.render(<App />, document.querySelector('.container'));
